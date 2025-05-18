cd86de2b3af6a26aeb3f891477314a325ccf0981
Vladimir Mikryukov <vmikryukov@gmail.com>
2025-05-18 18:20:58 +0300

    upd rao docs

--- R/raoQ.R
+++ R/raoQ.R
@@ -1,11 +1,12 @@
-#' Rao Quadratic
+#' Rao's Quadratic Entropy
 #'
-#' TODO
+#' Computes Rao's Quadratic Entropy, 
+#' a measure of diversity that accounts for both element abundances and their dissimilarities.
 #'
-#' @param ab 1 x n vector of abundances.
-#' @param diss n x n matrix with dissimilarities / distances
+#' @param ab A numeric vector of element abundances. Each element represents the abundance of an entity.
+#' @param diss A numeric matrix representing the dissimilarities or distances between entities. It should be a square matrix with dimensions equal to the length of `ab`.
 #'
-#' @return Q.
+#' @return A numeric value representing Rao's Quadratic Entropy.
 #' @export
 raoQuadratic <- function(ab, diss) {
 
