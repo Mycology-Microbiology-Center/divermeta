792544c3044ed7f9f0b9c27609bd9fbf2eea7614
Vladimir Mikryukov <v.mikryukov@gmail.com>
2025-10-23 19:30:37 +0300

    upd `divermeta` tests

--- tests/testthat/test-divermeta.R
+++ tests/testthat/test-divermeta.R
@@ -53,3 +53,53 @@ test_that("single index multiplicity_inventory works and matches direct", {
   })
   expect_equal(res$multiplicity_inventory, as.numeric(expected))
 })
+
+
+test_that("multiple indices and alias mapping; matches direct functions", {
+  fx <- make_fixture()
+  sig <- 0.8
+  q <- 1
+
+  res <- divermeta(fx$abund,
+    diss = fx$diss,
+    indices = c("M_inventory", "raoQ", "FD_sigma", "redundancy", "M_distance", "FDq"),
+    clusters = fx$clusters,
+    q = q,
+    sig = sig)
+
+  # Column names are normalized
+  expect_identical(colnames(res), c("Sample", "multiplicity_inventory", "raoQ", "FD_sigma", "redundancy", "multiplicity_distance", "FD_q"))
+
+  # Expected values per column
+  guard <- function(f) function(col) { if (sum(col) <= 0) return(NA_real_); f(col) }
+
+  expected_mi <- apply(fx$abund, 2, guard(function(col) multiplicity.inventory(col, fx$clusters, q = q)))
+  expected_rao <- apply(fx$abund, 2, guard(function(col) raoQuadratic(col, fx$diss)))
+  expected_fd_sigma <- apply(fx$abund, 2, guard(function(col) diversity.functional(col, fx$diss, sig = sig)))
+
+  ids <- rownames(fx$abund)
+  ut_idx <- which(upper.tri(fx$diss), arr.ind = TRUE)
+  diss_frame <- data.frame(
+    ID1 = ids[ut_idx[, 1]],
+    ID2 = ids[ut_idx[, 2]],
+    Distance = fx$diss[upper.tri(fx$diss)],
+    stringsAsFactors = FALSE
+  )
+  expected_md <- apply(fx$abund, 2, guard(function(col) multiplicity.distance.by_blocks(
+    ids = ids,
+    ab = col,
+    diss_frame = diss_frame,
+    clust = fx$clusters,
+    sigma = sig
+  )))
+
+  expected_fd_q <- apply(fx$abund, 2, guard(function(col) diversity.functional.traditional(col, fx$diss, q = q)))
+
+  expect_equal(res$multiplicity_inventory, as.numeric(expected_mi))
+  expect_equal(res$raoQ, as.numeric(expected_rao))
+  expect_equal(res$FD_sigma, as.numeric(expected_fd_sigma))
+  expect_equal(res$multiplicity_distance, as.numeric(expected_md))
+  expect_equal(res$FD_q, as.numeric(expected_fd_q))
+})
+
+
